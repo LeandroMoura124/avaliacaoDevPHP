b7e175eefa02b3bd9ac2f7ed3e739280cbdd3ebd
Leandro.Moura124 <leofcb334@gmail.com>
2023-06-02 18:05:24 -0300

    commit fornecendo o script do banco

---
+++ script_bancoPHP.sql
@@ -0,0 +1,10 @@
+/*Script do banco avaliacao Desenvolvedor PHP*/
+
+create table usuarios(
+id integer AUTO_INCREMENT,
+nome varchar(255),
+email varchar(255), 
+senha varchar(255),
+PRIMARY KEY(id)
+
+);
